7e858bb057008420a8c0580d39d5b5706e42ca15
JoshBenPhilip <joshben4c@gmail.com>
2022-04-22 14:49:46 -0400

    made working arrayElementSumFunction

---
+++ arrayElementSumFunction.js
@@ -0,0 +1,12 @@
+
+function sum(arr) {
+    let sum = 0;
+    for (let i = 0; i<arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log (sum);
+}
+
+let Array = [1, 2, 3, 4]
+
+console.log(sum (Array))
