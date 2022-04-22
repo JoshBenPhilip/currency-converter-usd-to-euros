7bc7628be97b640095acd09036897f557062ab4a
JoshBenPhilip <joshben4c@gmail.com>
2022-04-22 11:18:55 -0400

    first commit built working currency converter

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// let usd = 9
+
+//  function convertToEuro (usDollar)
+//  { return usDollar * .93}
+
+// console.log( convertToEuro (9))
+
+let usd = 9
+
+ function convertToEuro (usDollar)
+ { return usDollar * .93}
+
+console.log( convertToEuro (9))
+
